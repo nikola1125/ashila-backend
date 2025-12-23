77b1a29da8107f3f0fe22be02593e41a66d729f4
nikola <nhaxhi8@gmail.com>
2025-12-23 01:19:12 +0100

    firewall fix

--- server.js
+++ server.js
@@ -99,7 +99,9 @@ app.get('/debug-email', async (req, res) => {
   }
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: { user, pass }
   });
 
